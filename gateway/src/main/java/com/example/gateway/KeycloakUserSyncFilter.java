a754c39c6a7db6c3ad55d1a7321a8879aac0e4b6
Yash Patil <yashkhandagade@gmail.com>
2026-07-06 22:28:27 +0530

    Keycloak

--- gateway/src/main/java/com/example/gateway/KeycloakUserSyncFilter.java
+++ gateway/src/main/java/com/example/gateway/KeycloakUserSyncFilter.java
@@ -1,9 +1,15 @@
 package com.example.gateway;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,21 +24,62 @@ public class KeycloakUserSyncFilter implements WebFilter{
 	
 	private final UserService userService;
 	
-	
-	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain){
-	String userId = exchange.getRequest().getHeaders().getFirst("X-User-Id");
-	String token = exchange.getRequest().getHeaders().getFirst("Authorization");
-	RegisterRequest request = getUserDetails(token);
-	
-	
-	if(userId == null) {
-		userId = request.getKeycloakId();
-	}
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+        String userId = exchange.getRequest().getHeaders().getFirst("X-User-ID");
+        RegisterRequest registerRequest = getUserDetails(token);
+
+        if (userId == null) {
+            userId = registerRequest.getKeycloakId();
+        }
+
+        if (userId != null && token != null){
+            String finalUserId = userId;
+            return userService.validateUser(userId)
+                    .flatMap(exist -> {
+                        if (!exist) {
+                            // Register User
+
+                            if (registerRequest != null) {
+                                return userService.registerUser(registerRequest)
+                                        .then(Mono.empty());
+                            } else {
+                                return Mono.empty();
+                            }
+                        } else {
+                            log.info("User already exist, Skipping sync.");
+                            return Mono.empty();
+                        }
+                    })
+                    .then(Mono.defer(() -> {
+                    	return chain.filter(
+                    		    exchange.mutate()
+                    		            .request(builder ->
+                    		                builder.header("X-User-ID", finalUserId)
+                    		            )
+                    		            .build());
+                    }));
+        }
+        return chain.filter(exchange);
+    } 
+
+
+    private RegisterRequest getUserDetails(String token) {
+        try {
+            String tokenWithoutBearer = token.replace("Bearer ", "").trim();
+            SignedJWT signedJWT = SignedJWT.parse(tokenWithoutBearer);
+            JWTClaimsSet claims = signedJWT.getJWTClaimsSet();
+
+            RegisterRequest registerRequest = new RegisterRequest();
+            registerRequest.setEmail(claims.getStringClaim("email"));
+            registerRequest.setKeycloakId(claims.getStringClaim("sub"));
+            registerRequest.setPassword("dummy@123123");
+            registerRequest.setFirstName(claims.getStringClaim("given_name"));
+            registerRequest.setLastName(claims.getStringClaim("family_name"));
+            return registerRequest;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
-
-
-	private RegisterRequest getUserDetails(String token) {
-		try {
-			String tokenWithoutBearer = token.replace("Bearer ", "");
-		}
-	}
