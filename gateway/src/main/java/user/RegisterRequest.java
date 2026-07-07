17d510a2ce4621d33c0f6181d68ec591070317b6
Yash Patil <yashkhandagade@gmail.com>
2026-07-07 06:37:53 +0530

    keycloak

--- gateway/src/main/java/user/RegisterRequest.java
+++ gateway/src/main/java/user/RegisterRequest.java
@@ -1,20 +1,23 @@
 package user;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
-	
-	@NotBlank(message = "Email is required")
-	private String email;
-	
-	private String FName;
-	private String LName;
-	private String keycloakId;
-	
-	@NotBlank(message = "Password is required")
-	@Size(min = 6, message = "Password must be at least 6 characters long")
-	private String password;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must have atleast 6 characters")
+    private String password;
+
+    private String keycloakId;
+
+    private String firstName;
+    private String lastName;
+
 }
