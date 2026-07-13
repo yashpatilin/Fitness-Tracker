3ef616a46ff9568251d6340cbb40a5c12cc2508d
Yash Patil <yashkhandagade@gmail.com>
2026-07-13 11:05:11 +0530

    frontend

---
+++ ai-fitness-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Button } from "@mui/material"
+import {BrowserRouter as Router, Navigate, Route, Routes, useLocation} from "react-router"
+
+function App() {
+
+  return (
+    <Router>
+      <Button variant="contained">
+       LOGIN
+      </Button>
+    </Router>
+  )
+}
+
+export default App
