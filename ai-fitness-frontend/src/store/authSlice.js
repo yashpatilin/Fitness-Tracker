8bbf4add57b49c53a5774753cf37486abf021ca6
Yash Patil <yashkhandagade@gmail.com>
2026-07-14 23:40:33 +0530

    Frontend authentication with keycloak

--- ai-fitness-frontend/src/store/authSlice.js
+++ ai-fitness-frontend/src/store/authSlice.js
@@ -10,9 +10,23 @@ const authSlice = createSlice({
   reducers: {
     setCredentials: (state, action) => {
       state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.userId = action.payload.user.sub;
+
+      localStorage.setItem('token', action.payload.token);
+      localStorage.setItem('user', JSON.stringify(action.payload.user));
+      localStorage.setItem('userId', action.payload.user.sub);
+
     },
     logout: (state) => {
-    
+    state.user = null;
+      state.token = null;
+      state.userId = null;
+
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      localStorage.removeItem('userId');
+
     },
   },
 })
