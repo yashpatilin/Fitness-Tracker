b8fdec0fb813e4ee063dfe90359566dd9592d6af
Yash Patil <yashkhandagade@gmail.com>
2026-07-17 23:59:47 +0530

    Frontend

--- ai-fitness-frontend/src/App.jsx
+++ ai-fitness-frontend/src/App.jsx
@@ -6,6 +6,12 @@ import { AuthContext } from "react-oauth2-code-pkce"
 import {BrowserRouter as Router, Navigate, Route, Routes, useLocation} from "react-router"
 import { setCredentials } from "./store/authSlice";
 
+const ActivitiesPage = () => {
+  return (<Box sx={{ p: 2, border: '1px dashed grey' }}>
+      <ActivityForm/>
+      <ActivityList/>
+    </Box>);
+}
 function App() {
 
   const {token, tokenData, logIn, logOut, isAuthenticate} = useContext(AuthContext);
@@ -26,7 +32,15 @@ function App() {
       </Button>
   ) : (
     <div>
-    
+    <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+      <Button variant = "contained" onClick={logOut}>LOGOUT</Button>
+    </Box>
+    <Routes>
+      <Route path="/activities" element={<ActivitiesPage/>}/>
+      <Route path="/activities/:id" element={<ActivitiesDetail/>}/>
+      <Route path="/" element={token ? <Navigate to="/activities" replace/>:
+            <div>Welcome! Please login</div>}/>
+    </Routes>
     </div>
   )}
     </Router>
